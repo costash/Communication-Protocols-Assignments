dc8fc2a0acf93fbd98d6fefca9f279f0ce7176f7
Constantin Serban-Radoi <costashsrc@gmail.com>
2012-05-19 11:34:52 +0300

    I haz smth in that new function

--- my_dns_client.cpp
+++ my_dns_client.cpp
@@ -56,6 +56,55 @@ void get_host_by_name(unsigned char *host, unsigned char *server, int query_type
 	struct sockaddr_in serv_addr;
 	int sockfd;						// Socket for UDP connection
 	unsigned char buffer[BUFLEN], *queryname;
+
+	dns_header_t *dns_header = NULL;	// DNS Header
+	dns_question_t *dns_question = NULL;	// DNS Question
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(DNSPORT);
+	inet_aton((char *)server, &serv_addr.sin_addr);	// DNS address
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+
+	dns_header = (dns_header_t *) buffer;
+	memset(dns_header, 0, sizeof(dns_header));
+
+	dns_header->id = (unsigned short) htons(getpid());
+	dns_header->qdcount = htons(1);
+	dns_header->rd = 1;
+
+	cerr << endl << ntohs(dns_header->id) << " " << ntohs(dns_header->qdcount) << " ";
+	cerr << (unsigned short int)dns_header->rd << endl;
+
+	queryname = (unsigned char*) &buffer[sizeof(dns_header_t)];
+
+	cerr << host << endl;
+	convert_to_dns(queryname, host);
+	cerr << "\n" << queryname << endl << host << endl;
+
+	dns_question = (dns_question_t *) &buffer[ sizeof(dns_header_t)
+	                                           + strlen((const char*)queryname) + 1 ];
+
+	dns_question->qclass = htons(1);	// IN class for Internet
+	dns_question->qtype = htons(query_type);
+
+	cerr << "qclas: " << ntohs(dns_question->qclass) << " qtype: "
+			<< ntohs(dns_question->qtype) << endl;
+
+	cerr << "Sending query for " << host << endl;
+	if (sendto(sockfd, (char *)buffer, sizeof(dns_header_t)
+			+ (strlen((const char*)queryname) + 1) + sizeof(dns_question_t),
+			0, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		perror("Error in sendto");
+	}
+	cerr << "Done sending\n";
+
+	fd_set readfds;
+	struct timeval tv;
+
 }
 
 int main(int argc, char *argv[])
@@ -116,6 +165,8 @@ int main(int argc, char *argv[])
 
 
 	cerr << "Query sent for: " << goo;
+
+	get_host_by_name((unsigned char*)argv[1], (unsigned char*)DNS, A);
 
 	return 0;
 }
